711b208a0b23faf57e566d5c63ef11815fca5920
kanchika-kapoor <kanchika.ai@gmail.com>
2024-04-07 00:14:08 +0530

    added pagination

--- src/features/coins/index.tsx
+++ src/features/coins/index.tsx
@@ -1,20 +1,67 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useGetCryptosMutation } from '../../services/cryptoApi'
+import {Link} from 'react-router-dom'
+import { CoinType } from '../../common/types'
+import Items from '../../components/Items'
 
 const Home = () => {
-  const [getCryptos, {data}] = useGetCryptosMutation()
+  const [page, setPage] = useState(1)
+  const [perPage, setPerPage] = useState<string>("10")
+  const [getCryptos, {data: cryptos}] = useGetCryptosMutation()
+  const pages = ["5","10","15","20","25","30"]
 
   useEffect(()=>{
     const fetchCoinData = async () => {
-      await getCryptos(0)
+      await getCryptos({page, perPage})
     }
     fetchCoinData()
-  },[])
+  },[page, perPage])
 
-  console.log(data)
+  const handlePage = (e: React.ChangeEvent<HTMLSelectElement>)=>{
+    setPerPage(e.target.value)
+  }
+
+  // console.log(data)
   return (
-    <div>
-      <h2>Crypto coinlst</h2>
+    <div className='max-w-screen-lg m-auto'>
+      <div>
+        <div className='flex flex-row justify-between items-center bg-gray-900 rounded-lg font-bold mx-4 my-8 py-3 px-4 shadow-sm'>
+          <p>#</p>
+          <p className=''>Coin</p>
+          <p>Price</p>
+          <p>24h</p>
+          <p className='hide-mobile'>Volume</p>
+          <p className='hide-mobile'>Market Cap</p>
+        </div>
+        {cryptos?.map((coin:CoinType)=>{
+          return (<Link to={`/coin/${coin.id}`} key={coin.id}>
+            <Items coin={coin}/>
+          </Link>)
+        })}
+        <div className='flex items-center justify-center'>
+          <button
+          onClick={()=>setPage(page=>page-1)}
+          disabled={page === 1? true:false}
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2'
+          >Prev</button>
+          <p className='text-lg font-bold'> {page}</p>
+          <button
+          onClick={()=>setPage(page=>page+1)}
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2'
+          >Next</button>
+          <div className="dropdown inline-block relative ml-8">
+            <span>Per Page:</span>
+            <select value={perPage} onChange={handlePage}>
+              {pages.map((page, index)=>{
+                return (
+                  <option key={index}>{page}</option>
+                )
+              })}
+            </select>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -9,7 +9,7 @@ export const cryptoApi = createApi({
         getCryptos: builder.mutation({
             query:({page,perPage})=>{
                 return {
-                    url: `v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`,
+                    url: `v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false&locale=en`,
                     method:'get'
                 }
             }
