c9b94cbc7e139ea100ad1e6064040207830f3e25
kanchika-kapoor <kanchika.ai@gmail.com>
2024-04-07 01:14:50 +0530

    add nav to home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {FaCoins} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-center text-center mt-8'>
-      <FaCoins className='text-5xl text-orange-200 mx-3'/>
-      <h1 className="text-5xl text-white-600 font-bold">
-        Crypto
-        <span className="text-center font-bold text-5xl text-green-300"> Coins</span>
-      </h1>
-    </div>
+    <Link to="/">
+      <div className='flex justify-center text-center mt-8'>
+        <FaCoins className='text-5xl text-orange-200 mx-3'/>
+        <h1 className="text-5xl text-white-600 font-bold">
+          Crypto
+          <span className="text-center font-bold text-5xl text-green-300"> Coins</span>
+        </h1>
+      </div>
+    </Link>
   )
 }
 
